baf3360a3b77336c17eddc947d257b65b51446ee
Trenten Alien <trentenberam@gmail.com>
2025-02-19 20:08:01 -0500

    git pull

--- parse_git_pull.sh
+++ parse_git_pull.sh
@@ -1,3 +1,17 @@
 set -e
-# cat git_pull_output.txt |grep -E '\|'
-cat git_pull_output.txt |grep -En 'changed' | cut -d' ' -f1-2 | cut -d':' -f 1,2
+# # cat git_pull_output.txt |grep -E '\|'
+
+# In awk, $1 represents the first field (the first word) of the current line.
+# By setting $1 to $1 (essentially assigning the value of the first field to itself), 
+# awk triggers a re-evaluation of the entire line. 
+# This forces awk to rebuild the line by concatenating the fields with the default output 
+# field separator (which is a single space).
+# Importantly, this step automatically trims leading and trailing whitespace from the line, 
+# because awk strips out the leading/trailing spaces when reassembling the fields.
+# The last 1 is a shorthand for a true condition in awk. 
+# In awk, any condition that evaluates to true triggers the action following it (or the default action, which is to print the line).
+# So, 1 means "always true," and thus awk will print each modified line.
+NUM_FILES=$(cat git_pull_output.txt |grep -E 'file' | cut -d 'f' -f 1 | awk '{$1=$1}1')
+
+FILES=$(cat git_pull_output.txt | grep -oE '\s?([0-9a-zA-Z_\-\/]+\.\w{1,6})' | sort -u | awk '{$1=$1}1')
+# cat git_pull_output.txt | grep -oE '[^ ]+$'
